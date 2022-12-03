4922ab47081a4d49178dd517a50925bfff4ddff7
igorminash <iminash@inbox.ru>
2022-12-03 19:07:17 +0300

    Добавлено решение задачи 43

---
+++ Zadacha43/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] massive = GetArray (10, 0, 100);
+
+Console.WriteLine($"[{String.Join(", ", massive)}]");
+
+GetRevers(massive);
+Console.WriteLine($"[{String.Join(", ", massive)}]");
+
+
+int [] GetArray (int size, int minValue, int maxValue){
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++){
+        array [i] = new Random().Next(minValue, maxValue +1);
+    }
+    return array;
+}
+
+
+void GetRevers (int [] array){
+    for (int i = 0; i < array.Length-1; i++){
+        for (int j = i+1; j < array.Length; j++){
+            if (array[i] > array[j]){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }   
+} 
