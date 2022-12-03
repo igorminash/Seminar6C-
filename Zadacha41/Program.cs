d870edce11530a9415b1f936654e67bb40fcfb32
igorminash <iminash@inbox.ru>
2022-12-03 18:12:31 +0300

    Добавлено решение задачи 41

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] massive = GetArray (5, -3, 10);
+
+int count = 0;
+
+foreach (int i in massive){
+    if (i > 0){
+        count = count + 1;
+    }
+}
+
+int [] GetArray (int size, int minValue, int maxValue){
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++){
+        array [i] = new Random().Next(minValue, maxValue +1);
+    }
+    return array;
+}
+
+Console.WriteLine($"В массиве [{String.Join(", ", massive)}] количество положительных чисел = {count}");
+
+
